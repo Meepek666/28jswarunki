1e15b2c014f614d2ed0aa0ec9776725e1ce32daa
AGradzka <angelika.gradziel@onet.pl>
2022-12-08 09:44:30 +0100

    index

---
+++ main.js
@@ -0,0 +1,8 @@
+let number = 12
+if (number > 10) {
+    console.log('Spełniony warunek: number > 10');
+} else if (number > 13) {
+    console.log('Spełniony warunek: number > 13');
+} else {
+    console.log('Żaden z powyższych warunków NIE jest spełniony');
+}
